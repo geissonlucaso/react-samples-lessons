8afa1b03ee9ace644147681fda998892ced38faa
Geisson Oliveira <geissonlucaso@gmail.com>
2025-04-18 18:59:04 -0300

    Creation of the React001 'CommentCards' project.

---
+++ ReactJoaoRibeiro/react001/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Comment from './Components/Comment'
+
+function App() {
+    return (
+        <>
+            <h1>Comments</h1>
+            <Comment />
+            <Comment />
+            <Comment />
+        </>
+    )
+}
+
+export default App
---
+++ ReactJoaoRibeiro/react001/src/Components/Comment.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Comment.css'
+
+function Comment() {
+    return (
+        <>
+            <section className='comment'>
+                <p className='title'>Titulo</p>
+                <p className='text'>Comentario</p>
+                <p className='author'>Autor</p>
+            </section>
+        </>
+    )
+}
+
+export default Comment
